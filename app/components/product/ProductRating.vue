86a9593aa54971745196fb26df8f4a816f2f9132
Nestor Vera <nestorvera@me.com>
2025-01-14 18:23:33 +0100

    Fix icons not always being displayed

--- app/components/product/ProductRating.vue
+++ app/components/product/ProductRating.vue
@@ -9,7 +9,7 @@ const { rating = 0, count = null, size = 14, hideCount = false } = defineProps<{
 
 <template>
   <div class="flex gap-1 items-center">
-    <Icon v-for="i in 5" :key="i" name="ion:star" :size="size + ''" :style="{ color: rating < i ? '#ccc' : '#FBBE24' }" />
+    <UIcon v-for="i in 5" :key="i" name="ion:star" :size :style="{ color: rating < i ? '#ccc' : '#fbbe24' }" />
     <span v-if="count && !hideCount" class="">({{ count }})</span>
   </div>
 </template>
